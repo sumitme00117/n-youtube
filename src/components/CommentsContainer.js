8cb0df20c028604e4c371a82763d487c8d469d78
Sumit Srivastava <sumitsrivastava@Sumits-MacBook-Air.local>
2024-02-18 10:05:39 +0530

    Initial Commit

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+
+const commentsData = [
+  {
+  name: "Sumit",
+  text: "This is the first comment",
+  replies: [
+    {
+      name: "Sumit",
+      text: "This is the first comment",
+      replies: [
+        {
+          name: "Sumit",
+          text: "This is the first comment",
+          replies: [
+            {
+              name: "Sumit",
+              text: "This is the first comment",
+              replies: [],
+            },
+          ],
+        },
+        {
+          name: "Sumit",
+          text: "This is the first comment",
+          replies: [],
+        },
+      ],
+    }
+  ]
+},
+  {
+  name: "Sumit",
+  text: "This is the first comment",
+  replies: [
+    {
+      name: "Sumit",
+      text: "This is the first comment",
+      replies: [
+        {
+          name: "Sumit",
+          text: "This is the first comment",
+          replies: [],
+        },
+        {
+          name: "Sumit",
+          text: "This is the first comment",
+          replies: [],
+        }
+      ],
+    }
+  ]
+},
+]
+
+const Comment = ({data}) => {
+  const {name, text, replies} = data
+
+  return (
+    <div className='flex shadow-sm bg-gray-100 p-2 rounded-lg my-2'>
+      <img className='w-12 h-12' alt="user" src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"/>
+      <div className='px-3'>
+        <p className='font-bold'>{name}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  )
+}
+
+const CommentsList = ({comments}) => {
+  return comments.map((comment, index) => (
+    <div key={index}>
+  <Comment data={comment}/>
+  <div className='pl-5 border border-l-black ml-5'>
+      <CommentsList comments={comment.replies}/>
+  </div>
+  </div>
+  )
+)}
+
+const CommentsContainer = () => {
+  return (
+    <div className='m-5 p-2'>
+      <h1 className='text-2xl font-bold'>Comments:</h1>
+      <CommentsList comments={commentsData}/>
+    </div>
+  )
+}
+
+export default CommentsContainer
